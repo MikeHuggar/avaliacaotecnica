ed2e382958cfbb5be744b3de267002a8105571c0
Rogério Freitas Mateus <37122947+MikeHuggar@users.noreply.github.com>
2025-02-20 16:27:03 -0300

    Add files via upload

--- Desafio 2/index.php
+++ Desafio 2/index.php
@@ -11,29 +11,16 @@
 <body>
     <h1>Horas comprometidas:</h1>
     <?php
-        //Queries the entire professor table.
-        $queryProf = "SELECT * FROM professor";
-        $resultProf = mysqli_query($connection, $queryProf);
+        //Since we may have a teacher with no hours/department, we use left joins
+        $queryHours = "SELECT IFNULL(SUM(horario.numhoras), 0) AS total, professor.nomeprof FROM professor
+                        LEFT JOIN profturma ON professor.codprof = profturma.codprof AND professor.coddepto = profturma.coddepto
+                        LEFT JOIN turma ON profturma.coddepto = turma.coddepto AND profturma.numdisc = turma.numdisc AND profturma.anosem = turma.anosem AND profturma.siglatur = turma.siglatur
+                        LEFT JOIN horario ON turma.coddepto = horario.coddepto AND turma.numdisc = horario.numdisc AND turma.anosem = horario.anosem AND turma.siglatur = horario.siglatur
+                        GROUP BY professor.nomeprof ORDER BY total DESC";
 
-        //For each professor, we query their work hours.
-        while($row = mysqli_fetch_assoc($resultProf)){
-            //Through INNER JOINs, we can get the SUM of each professor's work hours, assuming they have any.
-            //INNER JOIN on basically every PRIMARY KEY to make sure we get what we're looking for.
-            $queryHours = "SELECT SUM(horario.numhoras) FROM professor
-                            INNER JOIN profturma ON professor.codprof = profturma.codprof AND professor.coddepto = profturma.coddepto
-                            INNER JOIN turma ON profturma.coddepto = turma.coddepto AND profturma.numdisc = turma.numdisc AND profturma.anosem = turma.anosem AND profturma.siglatur = turma.siglatur
-                            INNER JOIN horario ON turma.coddepto = horario.coddepto AND turma.numdisc = horario.numdisc AND turma.anosem = horario.anosem AND turma.siglatur = horario.siglatur
-                            WHERE professor.codprof = ".$row['codprof'];
-
-            $resultHours = mysqli_query($connection, $queryHours);
-            $sumHours = mysqli_fetch_assoc($resultHours);
-            if($sumHours['SUM(horario.numhoras)'] == null){
-                //Turns NULL values into 0 for the purpose of actually displaying something.
-                $sumHours['SUM(horario.numhoras)'] = 0;
-            }
-            echo "Horas comprometidas de " . $row['nomeprof'] . ": " . $sumHours['SUM(horario.numhoras)'];
-            echo "<br>";
-
+        $resultHours = mysqli_query($connection, $queryHours);
+        while($row = mysqli_fetch_assoc($resultHours)){
+            echo "Horas comprometidas de " . $row['nomeprof'] . ": " . $row['total'] . "<br>";
         }
     ?>
     <h1>Listas de Salas:</h1>
